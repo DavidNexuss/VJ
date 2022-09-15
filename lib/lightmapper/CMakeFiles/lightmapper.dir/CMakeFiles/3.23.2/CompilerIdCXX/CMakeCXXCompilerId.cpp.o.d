56a3b877683759a2ffab4800537ebb3bc73301d5
davidnexus <dgtgar01@gmail.com>
2022-09-15 13:25:40 +0200

    Removes binaries

--- lib/lightmapper/CMakeFiles/lightmapper.dir/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-lib/lightmapper/CMakeFiles/lightmapper.dir/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/david/shared/Projects/ShambhalaMini/lib/lightmapper/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- lib/xatlas/CMakeFiles/xatlas.dir/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-lib/xatlas/CMakeFiles/xatlas.dir/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/david/shared/Projects/ShambhalaMini/lib/xatlas/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
