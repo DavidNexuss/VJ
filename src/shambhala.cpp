832267ccdf72ddab964519e671f32800b7525e3d
DavidNexuss <davidgt7d1@gmail.com>
2022-10-16 13:14:01 +0200

    Updates texture filtering

--- src/shambhala.cpp
+++ src/shambhala.cpp
@@ -364,7 +364,8 @@ GLuint device::createTexture(bool filter, bool clamp) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
                     GL_LINEAR_MIPMAP_LINEAR);
   } else {
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_NEAREST_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   }
   return textureId;
