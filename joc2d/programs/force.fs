0a3855a9761b5037923b18b6267fa52f359fa93a
DavidNexuss <davidgt7d1@gmail.com>
2022-11-06 16:44:44 +0100

    Small changes force

--- joc2d/programs/force.fs
+++ joc2d/programs/force.fs
@@ -5,7 +5,28 @@ uniform float uTime;
 
 void main() { 
   float t = 1.0 / max(1.0, uTime - vEmission);
-  t = t * t;
-color = vec4(vec3(t * 5.0) * vec3(0.5,1.2,0.5),t);
+  t = t * t * t  *t;
+color = vec4(vec3(t * 17.0) * vec3(0.1,0.3,0.8),t);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
